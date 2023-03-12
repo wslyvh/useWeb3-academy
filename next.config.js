08f80b1ac0d28a80578742809a30717e973de9b3
0xdeenz <0xdeenz@protonmail.com>
2023-03-12 22:12:21 +0100

    update to new bq-core

--- next.config.js
+++ next.config.js
@@ -4,10 +4,37 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 /* const { withSentryConfig } = require('@sentry/nextjs') */
 
+const fs = require("fs")
+const withPWA = require("next-pwa")
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const nextConfig = withPWA({
+    dest: "public",
+    disable: process.env.NODE_ENV === "development"
+})({
+    eslint: {
+        ignoreDuringBuilds: true
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+    env: {
+        ETHEREUM_URL: process.env.ETHEREUM_URL,
+        ETHEREUM_PRIVATE_KEY: process.env.ETHEREUM_PRIVATE_KEY,
+        CONTRACT_ADDRESS: process.env.CONTRACT_ADDRESS
+    },
+    publicRuntimeConfig: {
+        GROUP_ID: process.env.GROUP_ID
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                fs: false
+            }
+        }
+
+        return config
+    }
+})
 
 module.exports = nextConfig
 
